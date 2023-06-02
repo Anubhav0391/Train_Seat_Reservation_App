a828e0ecde5236241cadd807cea074faefbd503e
unknown <anubhavdixit268@gmail.com>
2023-06-02 19:53:24 +0530

    seats added to the database

--- Backend/routes/seats.routes.js
+++ Backend/routes/seats.routes.js
@@ -12,4 +12,22 @@ seatsRouter.get("/", async (req, res) => {
   }
 });
 
+seatsRouter.post("/add", async (req, res) => {
+  try {
+    let count=0,flag=0
+    for(let i=1;i<=Math.ceil(80/7);i++){
+      flag++
+      for(let j=1;j<=7;j++){
+        count++;
+        const ring = new SeatsModel({seat:count ,
+          row:flag ,
+          status: false});
+        await ring.save();
+      }
+    }
+    res.status(200).send({ "msg": "Product added successfully"});
+  } catch (err) {
+    res.status(400).send({ "msg": err.message });
+  }
+});
 module.exports = seatsRouter;
